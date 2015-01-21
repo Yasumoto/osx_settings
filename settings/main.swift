6dc9885f7654bddfbb5ceb7aba8304e68483b865
Joe Smith <jsmith@twitter.com>
2015-01-21 10:04:40 -0800

    Initial Commit

---
+++ settings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  settings
+//
+//  Created by Joseph Smith on 1/21/15.
+//  Copyright (c) 2015 bjoli. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
