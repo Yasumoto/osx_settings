c80a443382f8006088337a00e9ab8b460f770c2a
Joe Smith <jsmith@twitter.com>
2015-01-23 21:02:33 -0800

    Update commands to actually work

--- settings/main.swift
+++ settings/main.swift
@@ -11,26 +11,26 @@ import Foundation
 let SUDO = "/usr/bin/sudo"
 let NVRAM = "/usr/sbin/nvram"
 // Boot your system in verbose mode
-let VERBOSE_BOOT = [NVRAM, "boot-args='-v'"]
+let VERBOSE_BOOT = [NVRAM, "boot-args=-v"]
 // Disable the loud startup chime which indicates hardware self-check success
-let DISABLE_STARTUP_CHIME = [NVRAM, "-d", "SystemAudioVolume"]
+let DISABLE_STARTUP_CHIME = [NVRAM, "SystemAudioVolume=%80"]
 
 func runSudoCommand(commandLine: [String], sudoPassword: String) {
     let task = NSTask()
     task.launchPath = SUDO
     task.arguments = ["-S"] + commandLine
-    
+
     let output = NSPipe()
     task.standardError = output
     let input = NSPipe()
     task.standardInput = input
-    
+
     if let passwordData = sudoPassword.dataUsingEncoding(NSASCIIStringEncoding) {
         task.launch()
         input.fileHandleForWriting.writeData(passwordData)
         input.fileHandleForWriting.closeFile()
     }
-    
+
     task.waitUntilExit()
     let data = output.fileHandleForReading.readDataToEndOfFile()
     if let output: String? = NSString(data: data, encoding: NSUTF8StringEncoding) {
